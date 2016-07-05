f2b0ce4878e44a9aace50e6e825822f865a5d936
kenpcjr <kennethcooke@me.com>
2016-07-05 16:27:34 -0400

    Done.

---
+++ swift-stringIntFuncDoc-readme-KC.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let name = "Melissa"
+
+name.uppercaseString
+
+let nameUpperCased = name.uppercaseString
+
+print("\(name)...\(name)...\(nameUpperCased) WAKE UP!!!")
+
